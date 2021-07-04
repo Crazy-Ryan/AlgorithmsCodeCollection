445506bca8a85e17e431d66265bdfa15530ff590
Crazy-Ryan <ranwang1995@outlook.com>
2021-07-04 20:56:25 +0800

    add sample answer to the insertion sort problem

---
+++ src/com/sorting/insertionSort/SampleAnswer.java
@@ -0,0 +1,14 @@
+package com.sorting.insertionSort;
+
+public class SampleAnswer {
+    private static void sort(int[] a) {
+        int j;
+        for (int p = 1; p < a.length; p++) {
+            int tmp = a[p];
+            for (j = p; j > 0 && tmp < a[j - 1]; j--) {
+                a[j] = a[j - 1];
+            }
+            a[j] = tmp;
+        }
+    }
+}
