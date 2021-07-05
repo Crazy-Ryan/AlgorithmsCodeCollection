02733afb122ce4340a91178a3cd6e8221e22b976
Crazy-Ryan <ranwang1995@outlook.com>
2021-07-05 21:53:44 +0800

    add selection sort java implementation

---
+++ src/com/sorting/selectionSort/SelectionSort.java
@@ -0,0 +1,27 @@
+package com.sorting.selectionSort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    private static int[] sort(int[] input) {
+        int size = input.length;
+        for (int ind = 0; ind < size; ind++) {
+            int minIndex = ind;
+            for (int index = ind + 1; index < size; index++) {
+                if (input[index] < input[minIndex]) {
+                    minIndex = index;
+                }
+            }
+            int tmp = input[ind];
+            input[ind] = input[minIndex];
+            input[minIndex] = tmp;
+        }
+        return input;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {34, 8, 64, 51, 32, 21};
+        int[] result = SelectionSort.sort(input);
+        System.out.println(Arrays.toString(result));
+    }
+}
