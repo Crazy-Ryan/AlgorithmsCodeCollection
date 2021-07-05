9b4ed1dab7ebadc828b537b748a137b6a45f134d
Crazy-Ryan <ranwang1995@outlook.com>
2021-07-05 22:12:06 +0800

    add bubble sort java implementation

---
+++ src/com/sorting/bubbleSort/BubbleSort.java
@@ -0,0 +1,30 @@
+package com.sorting.bubbleSort;
+
+import com.sorting.selectionSort.SelectionSort;
+import java.util.Arrays;
+
+public class BubbleSort {
+    private static int[] sort(int[] input) {
+        int size = input.length;
+        for (int ind = 0; ind < size - 1; ind++) {
+            for (int index = 0; index < size - ind - 1; index++) {
+                if (input[index] > input[index + 1]) {
+                    swap(input, index, index + 1);
+                }
+            }
+        }
+        return input;
+    }
+
+    private static void swap(int[] arr, int index1, int index2) {
+        int tmp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = tmp;
+    }
+
+    public static void main(String[] args) {
+        int[] input = {34, 8, 64, 51, 32, 21};
+        int[] result = BubbleSort.sort(input);
+        System.out.println(Arrays.toString(result));
+    }
+}
